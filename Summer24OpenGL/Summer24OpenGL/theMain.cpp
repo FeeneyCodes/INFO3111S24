5a2fad24f2e75002a070d426daaddc1b198e5f9c
michaelfeeney <michaelwcfeeney@gmail.com>
2024-05-09 14:21:50 -0400

    Loading a cow

--- Summer24OpenGL/Summer24OpenGL/theMain.cpp
+++ Summer24OpenGL/Summer24OpenGL/theMain.cpp
@@ -13,30 +13,97 @@
 #include <stdio.h>
 
 #include <iostream>
-#include <Windows.h>
+#include <string>
+#include <fstream>
 
 
-static const struct
+
+struct sVertex
 {
-    float x, y;
-    float r, g, b;
-} vertices[3] =
-{
-    { -0.6f, -0.4f, 1.f, 0.f, 0.f },
-    {  0.6f, -0.4f, 0.f, 1.f, 0.f },
-    {   0.f,  0.6f, 0.f, 0.f, 1.f }
+    float x, y, z;         // Positions
+    float r, g, b;          // Colours
 };
+
+sVertex vertices[3] =
+{
+    { -0.6f, -0.8f, 0.0f, 1.0f, 0.0f, 1.0f },   // B.R.
+    {  0.6f, -0.4f, 0.0f, 0.0f, 1.0f, 0.0f },   // B.L.
+    {  0.0f,  3.0f, 0.0f, 0.0f, 0.0f, 1.0f }    // TOP
+};
+
+bool loadPLY_XYZ_Format(std::string fileName)
+{
+    std::ifstream theFile(fileName.c_str());
+
+    std::string curWord;
+
+    std::cout << "Type in vertex to win!";
+    while ( theFile >> curWord )
+    {
+        if ( curWord == "vertex" )
+        {
+            break;
+        }
+    }
+    // element vertex 2903
+    unsigned int numVertices = 0;
+    theFile >> numVertices;
+
+
+    while (theFile >> curWord)
+    {
+        if (curWord == "face")
+        {
+            break;
+        }
+    }
+    // element face 5804
+    unsigned int numTriangles = 0;
+    theFile >> numTriangles;
+
+
+    while (theFile >> curWord)
+    {
+        if (curWord == "end_header")
+        {
+            break;
+        }
+    }
+    
+    // Start to read the vertices...
+    for ( unsigned int count = 0; count < numVertices; count++ )
+    {
+        //
+    }
+
+    //  4.13317 2.17523 1.25932 
+    //  4.23234 1.90308 0.534362
+    //  3 250 251 210
+    //  3 250 210 252
+
+    for (unsigned int count = 0; count < numTriangles; count++)
+    {
+        //
+    }
+
+
+
+    theFile.close();
+
+    return true;
+}
+
 
 static const char* vertex_shader_text =
 "#version 110\n"
 "uniform mat4 MVP;\n"
-"attribute vec3 vCol;\n"
-"attribute vec2 vPos;\n"
+"attribute vec3 vColour;\n"
+"attribute vec3 vPosition;\n"
 "varying vec3 color;\n"
 "void main()\n"
 "{\n"
-"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
-"    color = vCol;\n"
+"    gl_Position = MVP * vec4(vPosition, 1.0);\n"
+"    color = vColour;\n"
 "}\n";
 
 static const char* fragment_shader_text =
@@ -60,14 +127,15 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 
 int main(void)
 {
-    MessageBox(NULL, L"I love OpenGL", L"Hey!", MB_OK);
+ //   MessageBox(NULL, L"I love OpenGL", L"Hey!", MB_OK);
 
-
-    void* pMSGBOX = GetProcAddress(NULL, "MessageBoxW");
 
     GLFWwindow* window;
     GLuint vertex_buffer, vertex_shader, fragment_shader, program;
-    GLint mvp_location, vpos_location, vcol_location;
+    GLint mvp_location;
+
+    GLint vpos_location;
+    GLint vcol_location;
 
     glfwSetErrorCallback(error_callback);
     if (!glfwInit())
@@ -90,9 +158,21 @@ int main(void)
     gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
     glfwSwapInterval(1);
 
+    if ( loadPLY_XYZ_Format("assets/models/cow.ply") )
+    {
+        std::cout << "Cow loaded OK." << std::endl;
+    }
+    else
+    {
+        std::cout << "ALL IS LOST!! FOREVER LOST!!!" << std::endl;
+        // We're outta here
+        return -1;
+    }
+
     // NOTE: OpenGL error checks have been omitted for brevity
     glGenBuffers(1, &vertex_buffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    // Update this line here...
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     vertex_shader = glCreateShader(GL_VERTEX_SHADER);
@@ -108,16 +188,58 @@ int main(void)
     glAttachShader(program, fragment_shader);
     glLinkProgram(program);
 
-    mvp_location = glGetUniformLocation(program, "MVP");
-    vpos_location = glGetAttribLocation(program, "vPos");
-    vcol_location = glGetAttribLocation(program, "vCol");
 
+    mvp_location = glGetUniformLocation(program, "MVP");
+
+    vpos_location = glGetAttribLocation(program, "vPosition");  // 17
+    vcol_location = glGetAttribLocation(program, "vColour");
+
+//   struct sVertex
+//   {
+//       float x, y;         // Positions
+//       float r, g, b;      // Colours
+//   };
+//
+//   sVertex vertices[3] =
+//   {
+//       { -0.6f, -0.8f, 1.0f, 0.0f, 1.0f },   // B.R.
+//       {  0.6f, -0.4f, 0.0f, 1.0f, 0.0f },   // B.L.
+//       {  0.0f,  3.0f, 0.0f, 0.0f, 1.0f }    // TOP
+//   };
+// 
+//  
+
+
+    // attribute vec2 vPosition;
     glEnableVertexAttribArray(vpos_location);
-    glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
-                          sizeof(vertices[0]), (void*)0);
+    glVertexAttribPointer(vpos_location,        // Index: vPosition
+                          3,    // (now xyz)        // 2, 
+                          GL_FLOAT, 
+                          GL_FALSE,             // "normalized" or "clamped" -1 to 1
+                          sizeof(sVertex),      // Stride
+                          (void*)offsetof(sVertex, x) );            // Offset
+//                          sizeof(vertices[0]), 
+//                         (void*)0);            // Offset
+
+//    int x = 656;
+//    int* pX = &x;       // Pointer to x (type: integer pointer)
+//    void* pOMG = &x;    // Type is a pointer to whatever
+//    pOMG = key_callback;
+//    std::cout << *(std::string*)(pOMG);
+//    std::cout << (int*)(pOMG);
+
+    // attribute vec3 vColour;
     glEnableVertexAttribArray(vcol_location);
-    glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
-                          sizeof(vertices[0]), (void*)(sizeof(float) * 2));
+    glVertexAttribPointer(vcol_location,        // Index: vColour
+                          3, 
+                          GL_FLOAT, 
+                          GL_FALSE,
+                          sizeof(sVertex),      // Stride
+                          (void*)offsetof(sVertex, r) );    // Offset
+//                          sizeof(vertices[0]), 
+//                          (void*)(sizeof(float) * 2));
+
+   
     while (!glfwWindowShouldClose(window))
     {
         float ratio;
@@ -137,7 +259,7 @@ int main(void)
                                         (float)glfwGetTime(),
                                         glm::vec3(0.0f, 0.0, 1.0f));
 
-        m = m * rotateZ;
+//        m = m * rotateZ;
 
         //mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
         p = glm::perspective(0.6f,
